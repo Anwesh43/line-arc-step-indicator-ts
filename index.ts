b9506b70ee7ae12b09886fe89d897287d596d23d
Anwesh43 <anweshthecool0@gmail.com>
2020-03-22 03:51:07 +0530

    adding constatns

---
+++ index.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth
+const h : number = window.innerWidth
+const scGap : number = 0.02
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#4a148c"
+const backColor : string = "#bdbdbd"
+const nodes : number = 5
+const arcs : number = 6
+const delay : number = 20
