1110d0fd7f4a135e8d27f1c9e9e691a718b53496
Anwesh43 <anweshthecool0@gmail.com>
2020-03-22 04:25:28 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -23,3 +23,58 @@ class ScaleUtil {
         return Math.sin(Math.PI * scale)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1  : number, x2 : number, y2 : number)  {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArc(context : CanvasRenderingContext2D, r : number, start : number, deg : number) {
+        context.save()
+        context.rotate(deg * Math.PI / 180)
+        context.beginPath()
+        for (var i = 0; i < deg; i++) {
+            const x : number = r * Math.cos(i * Math.PI / 180)
+            const y : number = r * Math.sin(i * Math.PI / 180)
+            if (i == 0) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+        context.restore()
+    }
+
+    static drawLineArcIndicator(context : CanvasRenderingContext2D, i : number, scale : number, size : number) {
+        const gap : number = 2 * size / arcs
+        const sc : number = ScaleUtil.divideScale(scale, i, arcs)
+        const sf : number = ScaleUtil.sinify(sc)
+        const x : number = size - i * gap
+        const deg : number = 360 / arcs
+        DrawingUtil.drawLine(context, x, 0, x - size * sc, 0)
+        DrawingUtil.drawArc(context, size, i * deg, deg * sf)
+    }
+
+    static drawLineArcIndicators(context : CanvasRenderingContext2D, scale : number, size : number) {
+        for (var i = 0; i < arcs; i++) {
+            DrawingUtil.drawLineArcIndicator(context, i, scale, size)
+        }
+    }
+
+    static drawLAINode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = h / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.strokeStyle = foreColor
+        context.save()
+        context.translate(w / 2, gap * (i + 1))
+        DrawingUtil.drawLineArcIndicators(context, scale, size)
+        context.restore()
+    }
+}
